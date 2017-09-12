3aa97bd6be7374c22f276a5720944f1cbaed61b8
WesPain <w2112r@gmail.com>
2017-09-12 21:17:33 +0200

    Initial commit

---
+++ Temp_monitor.ino
@@ -0,0 +1,114 @@
+//define A1 1
+//define B1 2
+//define C1 3
+//define D1 4
+//define E1 5
+//define F1 6
+//define G1 7
+//define G3 8
+//define A2 9
+//define B2 10
+//define C2 11
+//define D2 12
+//define E2 13
+//define F2 14
+//define G2 15
+byte TenPlass = 10;
+byte TENseven_seg_digits[10][7] = { { 1,1,1,1,1,1,0 },  // = 0
+                                                           { 0,1,1,0,0,0,0 },  // = 1
+                                                           { 1,1,0,1,1,0,1 },  // = 2
+                                                           { 1,1,1,1,0,0,1 },  // = 3
+                                                           { 0,1,1,0,0,1,1 },  // = 4
+                                                           { 1,0,1,1,0,1,1 },  // = 5
+                                                           { 1,0,1,1,1,1,1 },  // = 6
+                                                           { 1,1,1,0,0,0,0 },  // = 7
+                                                           { 1,1,1,1,1,1,1 },  // = 8
+                                                           { 1,1,1,0,0,1,1 }   // = 9
+                                                           };
+byte seven_seg_digits[10][7] = { { 1,1,1,1,1,1,0 },  // = 0
+                                                           { 0,1,1,0,0,0,0 },  // = 1
+                                                           { 1,1,0,1,1,0,1 },  // = 2
+                                                           { 1,1,1,1,0,0,1 },  // = 3
+                                                           { 0,1,1,0,0,1,1 },  // = 4
+                                                           { 1,0,1,1,0,1,1 },  // = 5
+                                                           { 1,0,1,1,1,1,1 },  // = 6
+                                                           { 1,1,1,0,0,0,0 },  // = 7
+                                                           { 1,1,1,1,1,1,1 },  // = 8
+                                                           { 1,1,1,0,0,1,1 }   // = 9
+                                                           };
+int sensorpin =A5;
+int sensorValue=0;
+int LCDprint =0;
+int LCDTENprint =0;
+void setup() {
+  // put your setup code here, to run once:
+  //analogReference(INTERNAL);
+  Serial.begin (9600);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(14,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
+  //pinMode(A5,OUTPUT);
+  pinMode(16,OUTPUT);
+  //pinMode(8, OUTPUT);
+  pinMode(15,OUTPUT);
+  
+  
+
+}
+void sevenSegWrite(byte digit) {
+  byte pin = 10;
+  for (byte segCount = 0; segCount < 7; ++segCount) {
+    digitalWrite(pin, seven_seg_digits[digit][segCount]);
+    ++pin;
+  }
+}
+void TENsevenSegWrite(byte digit) {
+  byte pin = 2;
+  for (byte segCount = 0; segCount < 7; ++segCount) {
+    digitalWrite(pin, seven_seg_digits[digit][segCount]);
+    ++pin;
+  }
+}
+void loop() {
+  
+ int sensorValue = analogRead(sensorpin); //forklare
+  float voltage = (sensorValue * (5.0 / 1024))*100; //forlkare
+  LCDprint = (int) voltage%10; //forlkare
+  LCDTENprint = (int)voltage/10; 
+  delay(10);
+  sevenSegWrite(LCDprint);
+  TENsevenSegWrite(LCDTENprint);
+  delay(10);
+  //Buffere();
+  // print out the value you read:
+  Serial.println(voltage);
+  //Serial.println(LCDprint);
+  
+   
+ /*  for (byte count = 10; count > 0; --count){
+   delay(1000);
+   sevenSegWrite(count - 1);
+   TENsevenSegWrite(TenPlass - 1);
+   //*if(count-1==0){
+   TenPlass--;
+  }
+  delay(4000);*/
+
+}
+
+void Buffere(){
+  for(int i =0; i<17;++i){
+    digitalWrite(i,0);  
+  }
+}
+
